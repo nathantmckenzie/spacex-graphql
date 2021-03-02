ab20cf72be330a1c2258904f7724f604decd369d
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-02 11:58:49 -0800

    Added Client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.png";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          ></img>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
